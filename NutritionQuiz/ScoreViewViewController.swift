d0165f8cb55048567662e564de0c6406c941dec7
moooe429@gmail.com <Moe>
2023-08-15 18:06:08 +0900

    swift 1st commit

---
+++ NutritionQuiz/ScoreViewViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ScoreViewViewController.swift
+//  NutritionQuiz
+//
+//  Created by 墨田萌 on 2023/08/15.
+//
+
+import UIKit
+
+class ScoreViewViewController: UIViewController {
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var correct = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scoreLabel.text = "\(correct)問正解！"
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func toTopButtonAction(_ sender: Any) {
+        self.presentingViewController?.presentingViewController?.dismiss(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
