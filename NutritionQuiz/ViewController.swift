d04e7f762fb1e8ebdbcbd8fcfb328dd61df3855b
macaro-ni <moooe429@gmail.com>
2023-08-26 09:02:55 +0900

    [add]related to ATT

--- NutritionQuiz/ViewController.swift
+++ NutritionQuiz/ViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import AppTrackingTransparency  //追加
+import AdSupport  //追加
+
 
 class ViewController: UIViewController {
     @IBOutlet weak var startButton: UIButton!
@@ -17,6 +20,53 @@ class ViewController: UIViewController {
         startButton.layer.borderColor = UIColor.white.cgColor
     }
 
-
+//以下ATT対応
+    
+    //ライフサイクルて最後の方のやつ ↓↓↓
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //ATT対応
+        if #available(iOS 14, *) {
+            switch ATTrackingManager.trackingAuthorizationStatus {
+            case .authorized:
+                print("Allow Tracking")
+                print("IDFA: \(ASIdentifierManager.shared().advertisingIdentifier)")
+            case .denied:
+                print("拒否")
+            case .restricted:
+                print("制限")
+            case .notDetermined:
+                showRequestTrackingAuthorizationAlert()
+            @unknown default:
+                fatalError()
+            }
+        } else {// iOS14未満
+            if ASIdentifierManager.shared().isAdvertisingTrackingEnabled {
+                print("Allow Tracking")
+                print("IDFA: \(ASIdentifierManager.shared().advertisingIdentifier)")
+            } else {
+                print("制限")
+            }
+        }
+    }
+    
+    ///Alert表示
+        private func showRequestTrackingAuthorizationAlert() {
+            if #available(iOS 14, *) {
+                ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in
+                    switch status {
+                    case .authorized:
+                        print("🎉")
+                        //IDFA取得
+                        print("IDFA: \(ASIdentifierManager.shared().advertisingIdentifier)")
+                    case .denied, .restricted, .notDetermined:
+                        print("😥")
+                    @unknown default:
+                        fatalError()
+                    }
+                })
+            }
+        }
+    
 }
 
